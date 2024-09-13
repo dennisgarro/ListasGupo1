54a273074fefe290beb9aa278ed26354d6defd03
dennisgarro <sistemasitm6099@gmail.com>
2024-09-13 05:03:37 -0500

    exportar e importar archivos

---
+++ ExportarArchivo.java
@@ -0,0 +1,28 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.LinkedList;
+
+public class ExportarArchivo {
+
+    public void exportarArchivo(LinkedList<ObjProductos> productos) {
+        if (productos.isEmpty()) {
+            LLenarLista ll = new LLenarLista();
+            productos = ll.llenarlista(productos);
+        } else {
+            try (FileWriter escriba = new FileWriter("Producto.txt")) {
+                for (ObjProductos objProductos : productos) {
+                    escriba.write("Nombre: " + objProductos.getNombre() + "\n");
+                    escriba.write("Precio: " + objProductos.getPrecio() + "\n");
+                    escriba.write("Cantidad: " + objProductos.getCantidad() + "\n");
+                    escriba.write("---------------------------------------\n");
+
+                }
+
+                System.out.println("Archivo exportado correctamente");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
